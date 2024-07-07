577d985fe0296d02d4f5428848eb1d9ce972cc91
Júlia Mir Pedrol <mirp.julia@gmail.com>
2024-07-07 09:41:08 +0000

    install ohmyzsh and fish

--- setup_terminal.sh
+++ setup_terminal.sh
@@ -2,6 +2,7 @@
 
 # Install miniconda
 
+cd ~
 mkdir -p ~/Installed/miniconda3
 curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o ~/Installed/miniconda3/miniconda.sh
 bash ~/Installed/miniconda3/miniconda.sh -b -u -p ~/Installed/miniconda3
@@ -10,3 +11,11 @@ rm -rf ~/Installed/miniconda3/miniconda.sh
 ~/Installed/miniconda3/bin/conda init bash
 ~/Installed/miniconda3/bin/conda init zsh
 
+# Install oh-my-zsh
+
+cd ~/Installed
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install fish shell
+
+brew install fish
